bad998f0f0cc7ea145d384e5e429c5e65ab41a56
Talyssonbs <talyssonbs@gmail.com>
2022-07-06 10:05:42 -0300

    Aula 3| Novos caminhos e pastas

--- server.js
+++ server.js
@@ -15,7 +15,9 @@ servidor.listen(3000); */
 const http = require('http');
 
 const servidor = http.createServer(function (req, resp) {
-    resp.end(`
+    let html = "";
+    if (req.url == '/') {
+        html = `
         <html>
             <head>
                 <meta charset="utf-8">
@@ -24,6 +26,20 @@ const servidor = http.createServer(function (req, resp) {
                 <h1> Casa do Código </h1>
             </body> 
         </html>
-    `);
+    `;
+    } else if (req.url == "/livros") {
+        html = `
+        <html>
+            <head>
+                <meta charset="utf-8">
+            </head>
+            <body>
+                <h1> Listagem de Livros </h1>
+            </body> 
+        </html>
+    `;
+    }
+
+    resp.end(html);
 });
 servidor.listen(3000);
